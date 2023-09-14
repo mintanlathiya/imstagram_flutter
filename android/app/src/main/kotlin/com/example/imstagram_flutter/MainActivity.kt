01a39d0d02f84bee95862bc97574916b395037b9
mintanlathiya <lathiyamintan1998@gmail.com>
2023-09-14 23:21:45 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/imstagram_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imstagram_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:imstagram_flutter/Utils/colors.dart';
+import 'package:imstagram_flutter/providers/user_provider.dart';
+import 'package:imstagram_flutter/responsive/mobile_screen_layout.dart';
+import 'package:imstagram_flutter/responsive/responsive_layout_screen.dart';
+import 'package:imstagram_flutter/responsive/web_screen_layout.dart';
+import 'package:imstagram_flutter/screens/login_screen.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBSOsv_ydpOotcso3P13eHdYQU07engcds",
+          appId: "1:190447959931:web:4ea7dca85a3319e07e47de",
+          messagingSenderId: "190447959931",
+          projectId: "instagram-tut-2f6c9",
+          storageBucket: 'instagram-tut-2f6c9.appspot.com'),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              // Checking if the snapshot has any data or not
+              if (snapshot.hasData) {
+                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                return const ResponsiveLayout(
+                  mobileScreenLayout: MobileScreenLayout(),
+                  webScreenLayout: WebScreenLayout(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+
+            // means connection to future hasnt been made yet
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return const LoginScreen();
+          },
+        ),
+      ),
+    );
+  }
+}
